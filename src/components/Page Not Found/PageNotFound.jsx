a3f32d9ced0b8a9e9b8f306844aa58c0fe9e645b
PatilKhushal <patilkhushal2803@gmail.com>
2024-02-20 19:52:30 +0530

    hopefully this will solve route issue

--- src/components/Page Not Found/PageNotFound.jsx
+++ src/components/Page Not Found/PageNotFound.jsx
@@ -6,7 +6,7 @@ function PageNotFound() {
   return (
     <div className='h-dvh flex flex-col items-center justify-center gap-4'>
       <h1>Page Not Found</h1>
-      <p>{error.toString()}</p>
+      <p>{JSON.stringify(error)}</p>
       <Link to={'/'} className='bg-blue-500 px-4 py-2 rounded'>Homepage</Link>
     </div>
   )
